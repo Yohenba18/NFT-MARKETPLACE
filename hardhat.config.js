88557167b260200098a0d49549ec448ebb2e34be
Yohenba18 <roddik12kshetri@gmail.com>
2021-10-25 13:33:12 +0530

    Setup done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString()
+
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -10,5 +13,18 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 
 module.exports = {
+  network : {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [privateKey]
+    }
+  },
   solidity: "0.8.4",
 };
