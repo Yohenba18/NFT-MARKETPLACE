7f5e001669499379fc7344d10f7791a3699e9f01
Yohenba18 <roddik12kshetri@gmail.com>
2021-10-25 13:57:18 +0530

    created nftmarkket.sol

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.4;
 
-contract NFTMarket{
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
     
 }
